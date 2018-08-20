e52aecf0fd0ed8c2a8bd95c0acec63aebbc57dce
sauasmast <sdahal@caldwell.edu>
2018-08-20 16:03:16 -0400

    Made Everything working. Now to create a online resume and updated resume

--- index.js
+++ index.js
@@ -4,22 +4,15 @@ $(document).ready(function(){
        $('.js-animate-1').addClass('animated zoomIn');
     }, 500);
 
-    $(window).resize(function(){
-        alterClass();
-    });
-    
-    //Fire it when the page first loads:
-    alterClass();
+    var typed3 = new Typed('.js-type', {
+        strings: ['Glitch', 'Hi It\'s me:  Sauasmast','Hi It\'s me: Saugat', ' Software and Web Developer enthusiast', 'Check out my protfolio','Feedbacks are highly appreciated'],
+        typeSpeed: 100,
+        backSpeed: 100,
+        smartBackspace: true, // this is a default
+        loop: true
+      });
+        
 });
-
-var alterClass = function() {
-    var ww = document.body.clientWidth;
-    if (ww < 400) {
-        $('.test').removeClass('blue');
-    } else if (ww >= 401) {
-        $('.test').addClass('blue');
-      };
-    };
 
 /* For Scrolling to the specific points */
 $('.js-scroll-me').click(function(){
@@ -60,3 +53,40 @@ $('.js-animate-5').waypoint(function(direction){
 $('.js-animate-7').waypoint(function(direction){
     $('.contact .row:nth-child(3)>div').addClass('animated rotateIn');
 },{offset: '60%'});
+
+$('.js-animate-8').waypoint(function(direction){
+    $('.js-animate-8').addClass('animated fadeInUpBig');
+},{offset: '60%'});
+
+
+$('#feedback-btn').click(function(){
+    var Name= $('#name').val();
+    var Email= $('#email').val();
+    var Comment = $('#message').val();  
+      
+      if(Name=="" || Email=="" || Comment==""){
+        alert("You need to fill out all the requirements");
+      }
+      
+      else if(!(Email.includes('@'))){
+        alert("Please input the valid Email address.");
+      }
+      else{
+   $.ajax({
+          url: "./into.php", 
+          type:'POST',
+          data:{Name : Name,
+               Email : Email,
+               Comment: Comment},
+           success: function (){
+             $('#feedback-form').fadeOut(300, function()
+                                         { $(this).remove();
+                                         });
+              $('#change').hide().html("<h1> Thank you </h1>").fadeIn('slow');
+          },
+          error: function(jqXHR, textStatus, errorThrown) {
+             console.log(textStatus, errorThrown);
+          }            
+      });
+      }
+  });
