ae48cbb0f131ab3bce2070cd8de2756076c1c65e
sauasmast <sdahal@caldwell.edu>
2018-08-20 02:01:12 -0400

    Made everything reponsive

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
 /* For bouncing the logo */
 $(document).ready(function(){
      setTimeout(() => {
-       $('.js-animate-1').addClass('animated bounce');
-    }, 1000)
+       $('.js-animate-1').addClass('animated zoomIn');
+    }, 500);
+
+    $(window).resize(function(){
+        alterClass();
+    });
+    
+    //Fire it when the page first loads:
+    alterClass();
 });
 
+var alterClass = function() {
+    var ww = document.body.clientWidth;
+    if (ww < 400) {
+        $('.test').removeClass('blue');
+    } else if (ww >= 401) {
+        $('.test').addClass('blue');
+      };
+    };
 
 /* For Scrolling to the specific points */
 $('.js-scroll-me').click(function(){
@@ -22,3 +37,26 @@ $('.js-scroll-contact').click(function(){
 $('.js-scroll-open').click(function(){
     $('html,body').animate({scrollTop:$('.dirt').offset().top},1000);
 });
+
+/* Adding the animation on scroll with the waypoints */
+
+$('.js-animate-2').waypoint(function(direction){
+        $('.js-animate-2').addClass('animated fadeInDown')}
+        ,{offset: '75%'});
+
+$('.js-animate-3').waypoint(function(direction){
+    $('.js-animate-3').addClass('animated fadeIn')}
+    ,{offset: '75%'});
+
+$('.js-animate-4').waypoint(function(direction){
+    $('.js-animate-4').addClass('animated fadeInLeft')}
+    ,{offset: '75%'});
+
+$('.js-animate-5').waypoint(function(direction){
+    $('.js-animate-5').addClass('animated fadeInRight');
+    $('.big-line').addClass('animation');
+},{offset: '75%'});
+
+$('.js-animate-7').waypoint(function(direction){
+    $('.contact .row:nth-child(3)>div').addClass('animated rotateIn');
+},{offset: '60%'});
